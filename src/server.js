baf79c31e4aff59cb529124452ea1af6ec741f40
Valentin Parafioriti <vlp1025@rit.edu>
2022-08-31 12:47:43 -0400

    Added new json and js files

---
+++ src/server.js
@@ -0,0 +1,4 @@
+const http = require('http');
+const htmlHanlder = require('./htmlResponses.js');
+const textHanlder = require('./textResponses.js');
+const jsonHanlder = require('./jsonResponses.js');
